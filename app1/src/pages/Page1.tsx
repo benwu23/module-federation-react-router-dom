ff8a0895e9b90f72b9f97556625d445746ef1fa5
benwu23 <benwu23@cisco.com>
2025-07-24 13:46:11 -0700

    update app with useBlocker

--- app1/src/pages/Page1.tsx
+++ app1/src/pages/Page1.tsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Blocker, BlockerFunction, Link, useBlocker } from "react-router-dom";
+
+const ConfirmNavigation = ({ blocker }: { blocker: Blocker }) => {
+  if (blocker.state === "blocked") {
+    return (
+      <>
+        <p style={{ color: "red" }}>
+          Blocked the last navigation to {blocker.location.pathname}
+        </p>
+        <button onClick={() => blocker.proceed?.()}>Let me through</button>
+        <button onClick={() => blocker.reset?.()}>Keep me here</button>
+      </>
+    );
+  }
+
+  if (blocker.state === "proceeding") {
+    return (
+      <p style={{ color: "orange" }}>Proceeding through blocked navigation</p>
+    );
+  }
+
+  return <p style={{ color: "green" }}>Blocker is currently unblocked</p>;
+};
 
 export function Page1() {
+  const [value, setValue] = React.useState("");
+  // Allow the submission navigation to the same route to go through
+  const shouldBlock = React.useCallback<BlockerFunction>(
+    ({ currentLocation, nextLocation }) =>
+      value !== "" && currentLocation.pathname !== nextLocation.pathname,
+    [value]
+  );
+  const blocker = useBlocker(shouldBlock);
+
   return (
-    <React.Fragment>
+    <>
       <div>Page 1 from App1</div>
       <Link to="/page-2">Go to Page 2</Link>
-    </React.Fragment>
+      <br />
+      <input
+        name="data"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      {blocker.state === "blocked" && <ConfirmNavigation blocker={blocker} />}
+    </>
   );
 }
